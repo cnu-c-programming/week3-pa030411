bc7f4182b1da20aea736aeec5bb20d450e84eff1
박채영 <packcy030411@naver.com>
2026-03-22 13:17:07 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char type, int count, ...) {
+    va_list args;
+    va_start(args, count);
+    if (type == 'S') {
+        for (int i = 0; i < count; i++) {
+            char *str = va_arg(args, char *);
+            printf("%s ", str);
+        }
+        printf("\n");
+    } else if (type == 'C') {
+        for (int i = 0; i < count; i++) {
+            char ch = va_arg(args, int); // char is promoted to int
+            printf("%c", ch);
+        }
+        printf("\n");
+    } else if (type == 'D') {
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += va_arg(args, int);
+        }
+        printf("%d\n", sum);
+    }
+    va_end(args);
+}
+
+int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C', 'N', 'U');
+    my_sum('D', 3, 10, 20, 30);
+    return 0;
+}
